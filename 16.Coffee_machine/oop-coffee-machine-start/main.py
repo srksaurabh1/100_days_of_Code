f9caaa1402ed8df20fc89de37f1d22ede16a604a
srksaurbzz <srksaurbzz@gmail.com>
2022-07-06 18:41:41 +0530

    16

--- 16.Coffee_machine/oop-coffee-machine-start/main.py
+++ 16.Coffee_machine/oop-coffee-machine-start/main.py
@@ -3,4 +3,8 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
 money_machine.report()
+coffee_maker.report()
+
