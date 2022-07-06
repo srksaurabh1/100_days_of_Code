fb96c2f5d706a6ae02e0fd912365142eb41aecdb
srksaurbzz <srksaurbzz@gmail.com>
2022-07-06 17:25:37 +0530

    16

--- 16. Turtle/main.py
+++ 16. Turtle/main.py
@@ -1,1 +1,8 @@
-sgdg
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ['Pikachu', 'Squirtle'
+                                  ,'Charmander'])
+table.add_column("Type", ['Electric', 'Water', 'Fire'])
+
+
+print(table)
