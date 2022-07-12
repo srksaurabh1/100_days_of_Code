94083388afe04ee0943f664a72782c70c1f6d967
srksaurbzz <srksaurbzz@gmail.com>
2022-07-12 08:41:42 +0530

    snake

--- 20.Snake_game_01/main.py
+++ 20.Snake_game_01/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,46 +8,14 @@ screen.bgcolor('Black')
 screen.title('My Snake Game')
 screen.tracer(0)
 
+snake = Snake()
 
-# snake1 = Turtle(shape='square')
-# snake1.color('white')
-# snake2 = Turtle(shape='square')
-# snake2.color('white')
-# snake3 = Turtle(shape='square')
-# snake3.color('white')
-#
-# snake2.setposition(x=-20,y=0)
-# snake3.setposition(x=-40,y=0)
-
-
-starting_pos = [(0,0), (-20,0), (-40,0)]
-
-segments = []
-
-for position in starting_pos:
-    new_seqment = Turtle(shape='square')
-    new_seqment.color('white')
-    new_seqment.penup()
-    new_seqment.goto(position)
-    segments.append(new_seqment)
-
-screen.update()
 game_is_on = True
-
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) -1, 0, -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x,new_y)
-
-    segments[0].forward(20)
-    segments[0].left(90)
-
-
-
+    snake.move()
 
 
 
