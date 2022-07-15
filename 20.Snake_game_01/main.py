ae89df2172622e47aac9a3f9643cd27ea21a3735
srksaurbzz <srksaurbzz@gmail.com>
2022-07-15 14:06:20 +0530

    extend

--- 20.Snake_game_01/main.py
+++ 20.Snake_game_01/main.py
@@ -31,12 +31,18 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         score.increase_score()
 
     # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         score.game_over()
 
     # Detect collision with tail
+    elif snake.head.distance(snake.tail) < 15:
+
+        game_is_on = False
+        score.game_over()
+
 screen.exitonclick()
